5d233134bb73d29a03c074dacb56701be3bfb66c
samling <samlingx@gmail.com>
2018-03-30 19:03:34 -0700

    Adding new tmux commands to tab layer

--- Firmware/sam/keymap.c
+++ Firmware/sam/keymap.c
@@ -7,8 +7,27 @@
 #define HHKB 1
 #define SAM  2
 
+#define TMUX_PREFIX SS_LCTRL("B")
+
 enum custom_keycodes {
-    EXAMPLE_MACRO = SAFE_RANGE
+    EXAMPLE_MACRO = SAFE_RANGE,
+    //GRAVE_ESCAPE,
+    TMUX_PREV,
+    TMUX_NEXT,
+    TMUX_RENAME_TAB,
+    TMUX_NEW_TAB,
+    TMUX_SPLIT_PANE,
+    TMUX_KILL_PANE,
+    TMUX_W1,
+    TMUX_W2,
+    TMUX_W3,
+    TMUX_W4,
+    TMUX_W5,
+    TMUX_W6,
+    TMUX_W7,
+    TMUX_W8,
+    TMUX_W9,
+    TMUX_W0
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -69,15 +88,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 
     /* Layer SAM: SAM custom mode (HHKB Tab)
-      |------+-----+-----+-----+-----+----+----+----+-----+-----+-----+-----+-------+-------+-----|
-      |      |     |     |     |     |    |    |    |     |     |     |     |       |       |NUMLK|
-      |------+-----+-----+-----+-----+----+----+----+-----+-----+-----+-----+-------+-------+-----|
-      |(N/A) |     |     |     |     |    |    |    |NUM_7|NUM_8|NUM_9|     |       |       |     |
-      |------+-----+-----+-----+-----+----+----+----+-----+-----+-----+-----+-------+-------+-----|
-      |      |     |     |     |     |    |    |    |NUM_4|NUM_5|NUM_6|     |       |       |     |
-      |------+-----+-----+-----+-----+----+----+----+-----+-----+-----+-----+-------+-------+-----|
-      |      |     |     |     |     |    |    |    |NUM_1|NUM_2|NUM_3|     |       |       |     |
-      |------+-----+-----+-----+-----+----+----+----+-----+-----+-----+-----+-------+-------+-----|
+      |------+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+-------+-------+-----|
+      |      |tmux1|tmux2|tmux3|tmux4|tmux5|tmux6|tmux7|tmux8|tmux9|tmux0|     |       |       |NUMLK|
+      |------+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+-------+-------+-----|
+      |(N/A) |     |tmuxW|     |tmuxR|tmuxT|     |     |NUM_7|NUM_8|NUM_9|tmuxP|tmuxN  |       |     |
+      |------+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+-------+-------+-----|
+      |      |     |     |tmuxD|     |     |     |     |NUM_4|NUM_5|NUM_6|     |       |       |     |
+      |------+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+-------+-------+-----|
+      |      |     |     |     |     |     |     |     |NUM_1|NUM_2|NUM_3|     |       |       |     |
+      |------+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+-------+-------+-----|
 
                  |------+------+----------------------+------+------+
                  | **** | **** | ******************** |NUM_0 |NUM_. |
@@ -90,11 +109,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     */
 
   [SAM] = KEYMAP(
-  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,   KC_TRNS,   KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_NUMLOCK, \
-  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,   KC_TRNS,   KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_P7,    KC_P8,    KC_P9,    KC_TRNS,  KC_TRNS,  KC_TRNS,              \
-  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,   KC_TRNS,   KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_P4,    KC_P5,    KC_P6,    KC_TRNS,  KC_TRNS,                        \
-  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,   KC_TRNS,   KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_P1,    KC_P2,    KC_P3,    KC_TRNS,  KC_TRNS,                        \
-                        KC_TRNS, KC_TRNS,            KC_TRNS,                KC_P0, KC_PDOT)};
+  KC_TRNS,  TMUX_W1,  TMUX_W2,         TMUX_W3,           TMUX_W4,           TMUX_W5,          TMUX_W6,  TMUX_W7,  TMUX_W8,  TMUX_W9,  TMUX_W0,  KC_TRNS,         KC_TRNS,          KC_TRNS,  KC_NUMLOCK, \
+  KC_TRNS,  KC_TRNS,  TMUX_KILL_PANE,  KC_TRNS,           TMUX_RENAME_TAB,   TMUX_NEW_TAB,     KC_TRNS,  KC_TRNS,  KC_P7,    KC_P8,    KC_P9,    TMUX_PREV,       TMUX_NEXT,        KC_TRNS,              \
+  KC_TRNS,  KC_TRNS,  KC_TRNS,         TMUX_SPLIT_PANE,   KC_TRNS,           KC_TRNS,          KC_TRNS,  KC_TRNS,  KC_P4,    KC_P5,    KC_P6,    KC_TRNS,         KC_TRNS,                                \
+  KC_TRNS,  KC_TRNS,  KC_TRNS,         KC_TRNS,           KC_TRNS,           KC_TRNS,          KC_TRNS,  KC_TRNS,  KC_P1,    KC_P2,    KC_P3,    KC_TRNS,         KC_TRNS,                                \
+                                    KC_TRNS, KC_TRNS,            KC_TRNS,                KC_P0, KC_PDOT)};
 
 const uint16_t PROGMEM fn_actions[] = {
 
@@ -107,10 +126,73 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
             case EXAMPLE_MACRO:
                 SEND_STRING("Example string");
                 return false;
+            //case GRAVE_ESCAPE: // Send ` if super or alt are held down, end ESC if otherwise (or if shift is held down)
+            //    if ((keyboard_report->mods & (MOD_BIT(KC_LGUI))) || (keyboard_report->mods & (MOD_BIT(KC_LALT)))) {
+            //        SEND_STRING("`");
+            //    } else {
+            //        SEND_STRING(SS_TAP(X_ESCAPE));
+            //    }
+            //    return false;
+            case TMUX_PREV:        // C-b p
+                SEND_STRING(TMUX_PREFIX SS_TAP(X_P));
+                return false;
+            case TMUX_NEXT:        // C-b n
+                SEND_STRING(TMUX_PREFIX SS_TAP(X_N));
+                return false;
+            case TMUX_RENAME_TAB:  // C-b R
+                SEND_STRING(TMUX_PREFIX SS_DOWN(X_LSHIFT) SS_TAP(X_R) SS_UP(X_LSHIFT));
+                return false;
+            case TMUX_NEW_TAB:     // C-b T
+                SEND_STRING(TMUX_PREFIX SS_DOWN(X_LSHIFT) SS_TAP(X_T) SS_UP(X_LSHIFT));
+                return false;
+            case TMUX_KILL_PANE:   // C-b w
+                if (keyboard_report->mods & MOD_BIT(KC_LSHIFT)) { // Check if shift is pressed
+                    SEND_STRING(TMUX_PREFIX SS_DOWN(X_LSHIFT) SS_TAP(X_W) SS_UP(X_LSHIFT)); // Prompt to kill window
+                } else {
+                    SEND_STRING(TMUX_PREFIX SS_TAP(X_W) SS_TAP(X_Y)); // Kill pane with auto 'y'
+                }
+                return false;
+            case TMUX_SPLIT_PANE:  // C-b W
+                if (keyboard_report->mods & MOD_BIT(KC_LSHIFT)) { // Check if shift is pressed
+                    SEND_STRING(TMUX_PREFIX SS_DOWN(X_LSHIFT) SS_TAP(X_D) SS_UP(X_LSHIFT)); // Split pane horizontally
+                } else {
+                    SEND_STRING(TMUX_PREFIX SS_TAP(X_D)); // Split pane vertically
+                }
+                return false;
+            case TMUX_W1:          // C-b 1
+                SEND_STRING(TMUX_PREFIX SS_TAP(X_1));
+                return false;
+            case TMUX_W2:          // C-b 2
+                SEND_STRING(TMUX_PREFIX SS_TAP(X_2));
+                return false;
+            case TMUX_W3:          // C-b 3
+                SEND_STRING(TMUX_PREFIX SS_TAP(X_3));
+                return false;
+            case TMUX_W4:          // C-b 4
+                SEND_STRING(TMUX_PREFIX SS_TAP(X_4));
+                return false;
+            case TMUX_W5:          // C-b 5
+                SEND_STRING(TMUX_PREFIX SS_TAP(X_5));
+                return false;
+            case TMUX_W6:          // C-b 6
+                SEND_STRING(TMUX_PREFIX SS_TAP(X_6));
+                return false;
+            case TMUX_W7:          // C-b 7
+                SEND_STRING(TMUX_PREFIX SS_TAP(X_7));
+                return false;
+            case TMUX_W8:          // C-b 8
+                SEND_STRING(TMUX_PREFIX SS_TAP(X_8));
+                return false;
+            case TMUX_W9:          // C-b 9
+                SEND_STRING(TMUX_PREFIX SS_TAP(X_9));
+                return false;
+            case TMUX_W0:          // C-b 0
+                SEND_STRING(TMUX_PREFIX SS_TAP(X_0));
+                return false;
         }
     }
     return true;
-}
+};
 
 const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt)
 {
