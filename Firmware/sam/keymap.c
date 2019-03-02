edeae32371de05ba74f561195c67d5c8fd02b16b
Sam Boynton <sboynton@sams-mbp.lan>
2019-03-01 23:05:18 -0800

    Updating keymap to support new QMK version, adding spacecadet-style ctrl binding: esc when tapped, ctrl when held

--- Firmware/sam/keymap.c
+++ Firmware/sam/keymap.c
@@ -11,6 +11,7 @@
 
 enum custom_keycodes {
     EXAMPLE_MACRO = SAFE_RANGE,
+    SILLY_MACRO,
     //GRAVE_ESCAPE,
     TMUX_PREV,
     TMUX_NEXT,
@@ -53,12 +54,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         - Fn1 added on TAB (hold to engage, tap to send regular TAB)
     */
 
-  [BASE] = KEYMAP(  //  default layer
-  KC_ESC,           KC_1,  KC_2,  KC_3,  KC_4,  KC_5,    KC_6,  KC_7,  KC_8,     KC_9,    KC_0,     KC_MINS,  KC_EQL,    KC_BSLS,  KC_GRV, \
-  LT(SAM, KC_TAB),  KC_Q,  KC_W,  KC_E,  KC_R,  KC_T,    KC_Y,  KC_U,  KC_I,     KC_O,    KC_P,     KC_LBRC,  KC_RBRC,   KC_BSPC,          \
-  KC_LCTL,          KC_A,  KC_S,  KC_D,  KC_F,  KC_G,    KC_H,  KC_J,  KC_K,     KC_L,    KC_SCLN,  KC_QUOT,  KC_ENT,                      \
-  KC_LSFT,          KC_Z,  KC_X,  KC_C,  KC_V,  KC_B,    KC_N,  KC_M,  KC_COMM,  KC_DOT,  KC_SLSH,  KC_RSFT,  MO(HHKB),                    \
-                            KC_LALT,  KC_LGUI,  /*        */ KC_SPC,      KC_RGUI,  KC_RALT),
+  [BASE] = LAYOUT(  //  default layer
+  KC_ESC,                   KC_1,  KC_2,  KC_3,  KC_4,  KC_5,    KC_6,  KC_7,  KC_8,     KC_9,    KC_0,     KC_MINS,  KC_EQL,    KC_BSLS,  KC_GRV, \
+  LT(SAM, KC_TAB),          KC_Q,  KC_W,  KC_E,  KC_R,  KC_T,    KC_Y,  KC_U,  KC_I,     KC_O,    KC_P,     KC_LBRC,  KC_RBRC,   KC_BSPC,          \
+  MT(MOD_LCTL, KC_ESC),     KC_A,  KC_S,  KC_D,  KC_F,  KC_G,    KC_H,  KC_J,  KC_K,     KC_L,    KC_SCLN,  KC_QUOT,  KC_ENT,                      \
+  KC_LSFT,                  KC_Z,  KC_X,  KC_C,  KC_V,  KC_B,    KC_N,  KC_M,  KC_COMM,  KC_DOT,  KC_SLSH,  KC_RSFT,  MO(HHKB),                    \
+                                        KC_LALT,  KC_LGUI,  /*        */ KC_SPC,      KC_RGUI,  KC_RALT),
 
 
 
@@ -79,7 +80,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
      */
 
-  [HHKB] = KEYMAP(
+  [HHKB] = LAYOUT(
   KC_PWR,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,   KC_INS,   KC_DEL, \
   KC_CAPS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_PSCR,  KC_SLCK,  KC_PAUS,  KC_UP,    KC_TRNS,  KC_BSPC,          \
   KC_TRNS,  KC_VOLD,  KC_VOLU,  KC_MUTE,  KC_TRNS,  KC_TRNS,  KC_PAST,  KC_PSLS,  KC_HOME,  KC_PGUP,  KC_LEFT,  KC_RGHT,  KC_PENT,                    \
@@ -109,10 +110,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         - Numpad added to right-hand side
     */
 
-  [SAM] = KEYMAP(
+  [SAM] = LAYOUT(
   KC_TRNS,  TMUX_W1,  TMUX_W2,         TMUX_W3,           TMUX_W4,           TMUX_W5,          TMUX_W6,         TMUX_W7,  TMUX_W8,  TMUX_W9,  TMUX_W0,  KC_TRNS,         KC_TRNS,          KC_TRNS,  KC_NUMLOCK, \
   KC_TRNS,  KC_TRNS,  TMUX_KILL_PANE,  KC_TRNS,           TMUX_RENAME_TAB,   TMUX_NEW_TAB,     KC_TRNS,         KC_TRNS,  KC_P7,    KC_P8,    KC_P9,    TMUX_PREV,       TMUX_NEXT,        KC_TRNS,              \
-  KC_TRNS,  KC_TRNS,  KC_TRNS,         TMUX_SPLIT_PANE,   KC_TRNS,           KC_TRNS,          KC_TRNS,         KC_TRNS,  KC_P4,    KC_P5,    KC_P6,    KC_TRNS,         KC_TRNS,                                \
+  KC_TRNS,  KC_TRNS,  KC_TRNS,         TMUX_SPLIT_PANE,   SILLY_MACRO,           KC_TRNS,          KC_TRNS,         KC_TRNS,  KC_P4,    KC_P5,    KC_P6,    KC_TRNS,         KC_TRNS,                                \
   KC_TRNS,  KC_TRNS,  KC_TRNS,         KC_TRNS,           KC_TRNS,           KC_TRNS,          TMUX_RENUM_TAB,  KC_TRNS,  KC_P1,    KC_P2,    KC_P3,    KC_TRNS,         KC_TRNS,                                \
                                     KC_TRNS, KC_TRNS,            KC_TRNS,                KC_P0, KC_PDOT)};
 
@@ -126,6 +127,15 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         switch(keycode) {
             case EXAMPLE_MACRO:
                 SEND_STRING("Example string");
+                return false;
+            case SILLY_MACRO:
+                SEND_STRING("Hello NAME,");
+                SEND_STRING(SS_TAP(X_ENTER));
+                SEND_STRING("I just wanted to reach out to you PERSONALLY to let you know I'm giving your problem with PRODUCT my full attention. No one is as important as you, [NAME].");
+                SEND_STRING(SS_TAP(X_ENTER));
+                SEND_STRING("Thank you,");
+                SEND_STRING(SS_TAP(X_ENTER));
+                SEND_STRING("Sam");
                 return false;
             //case GRAVE_ESCAPE: // Send ` if super or alt are held down, end ESC if otherwise (or if shift is held down)
             //    if ((keyboard_report->mods & (MOD_BIT(KC_LGUI))) || (keyboard_report->mods & (MOD_BIT(KC_LALT)))) {
